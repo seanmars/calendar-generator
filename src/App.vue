51a6b14c0ebe05b24fd5d75f93bc2f850e611796
seanmars <mars0222@gmail.com>
2023-11-23 17:47:13 +0800

    初版完成

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
-import { NMessageProvider } from 'naive-ui';
+import { NConfigProvider, NMessageProvider } from 'naive-ui';
+import { zhTW, dateZhTW } from 'naive-ui';
 
 import GoogleCalenderGenerator from './components/GoogleCalenderGenerator.vue';
 </script>
 
 <template>
-  <n-message-provider>
-    <google-calender-generator />
-  </n-message-provider>
+  <n-config-provider :locale="zhTW" :date-locale="dateZhTW">
+    <n-message-provider>
+      <google-calender-generator />
+    </n-message-provider>
+  </n-config-provider>
 </template>
 
 <style scoped>
--- src/components/GoogleCalenderGenerator.vue
+++ src/components/GoogleCalenderGenerator.vue
@@ -1,93 +1,231 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
+import * as dayjs from 'dayjs';
+import 'dayjs/locale/zh-tw.js';
+import { google } from 'calendar-link';
 import {
-  NSpace,
-  NLayout,
-  NButton,
-  NInput,
-  NForm,
   FormInst,
+  NButton,
+  NCheckbox,
+  NDatePicker,
+  NForm,
+  NFormItem,
+  NInput,
+  NLayout,
+  NSelect,
+  NSpace,
   useMessage
 } from 'naive-ui';
 
+/*
+1.姓名
+2.時間(ex: 2023/11/22 0930-18:00，盡量不要請了又再更動)
+3.假別(特休、病假、事假...等等)
+4.事由(若不方便打出，請私敲主管)
+5.代理人(姓名，若不知道請詢問主管)
+* */
 interface InterfaceInput {
-  user: {
-    name: string;
-    age: string;
-  };
-  phone: string;
+  name: string;
+  leaveType: string;
+  reason: string;
+  agentName: string;
 }
 
-const formRef = ref<FormInst | null>(null);
+const Hour24: number = 86400000;
+const Hour8: number = 28800000;
+
+dayjs.locale('zh-tw');
+
 const message = useMessage();
-const formValue = ref({
-  user: {
-    name: '',
-    age: ''
-  },
-  phone: ''
+const timePickerProps = {
+  format: 'HH:mm',
+  timeZone: 'Asia/Taipei',
+  hours: [9, 12, 18],
+  minutes: [0, 30],
+};
+
+const formRef = ref<FormInst | null>(null);
+const formValue = reactive<InterfaceInput>({
+  name: '',
+  leaveType: '',
+  reason: '',
+  agentName: ''
 });
 
-const rules = {
-  user: {
-    name: {
-      required: true,
-      message: 'Please input your name',
-      trigger: 'blur'
-    },
-    age: {
-      required: true,
-      message: 'Please input your age',
-      trigger: ['input', 'blur']
-    }
+const leaveTypeOptions = [
+  {
+    label: '特休',
+    value: '特休',
+    disabled: false
   },
-  phone: {
-    required: true,
-    message: 'Please input your number',
-    trigger: ['input']
+  {
+    label: '病假',
+    value: '病假',
+    disabled: false
+  },
+  {
+    label: '事假',
+    value: '事假',
+    disabled: false
+  },
+  {
+    label: '其他',
+    value: '其他',
+    disabled: false
+  },
+];
+const rules = {};
+
+const isAllDay = ref<boolean>(false);
+const timestamp = ref<[number, number] | null>(null);
+const resultValue = ref<string>('');
+
+const handleSubmit = () => {
+  if (!formValue.name) {
+    message.error('請輸入請假人姓名');
+    return;
+  }
+
+  if (!timestamp.value || !timestamp.value[0] || !timestamp.value[1]) {
+    message.error('請選擇請假時間');
+    return;
+  }
+
+  if (!formValue.leaveType) {
+    message.error('請選擇假別');
+    return;
+  }
+
+  if (!formValue.agentName) {
+    message.error('請輸入代理人');
+    return;
+  }
+
+  const desc = `假別：${formValue.leaveType}\n事由：${formValue.reason}\n代理人：${formValue.agentName}`;
+
+  // use days to get start time
+  const startTime = dayjs(timestamp.value[0]);
+  const event = {
+    title: `${startTime.format('HH:mm')} ${formValue.name} ${formValue.leaveType}`,
+    description: desc,
+    start: new Date(timestamp.value[0]),
+    end: new Date(timestamp.value[1]),
+    busy: false
+  };
+
+  resultValue.value = google(event);
+};
+
+const handleMakeEvent = () => {
+  // open the url in a new window
+};
+
+const onIsAllDayChanged = (value: boolean): void => {
+  if (!timestamp.value) {
+    return;
+  }
+
+  if (value) {
+    const start = (timestamp.value[0] - (timestamp.value[0] - Hour8) % Hour24) + Hour8;
+    const end = (timestamp.value[1] - (timestamp.value[1] - Hour8) % Hour24) + Hour8;
+    timestamp.value = [start, end];
+  } else {
+    const start = timestamp.value[0];
+    const end = timestamp.value[1];
+    timestamp.value = [start, end];
   }
 };
 
-const handleSubmit = () => {
-  message.info('Button Clicked');
-};
 </script>
 
 <template>
-  <n-form
-      ref="formRef"
-      inline
-      :label-width="80"
-      :model="formValue"
-      :rules="rules"
-      size="large"
-  >
-    <n-layout content-style="padding: 24px;">
-      <n-layout :content-style="{'display': 'flex', 'align-items': 'center', 'justify-content': 'center'}">
-        <n-space vertical size="small" class="mainForm">
-          <n-form-item label="Name" path="user.name">
-            <n-input v-model:value="formValue.user.name" placeholder="Input Name" />
-          </n-form-item>
-          <n-form-item label="Age" path="user.age">
-            <n-input v-model:value="formValue.user.age" placeholder="Input Age" />
-          </n-form-item>
-          <n-form-item label="Phone" path="phone">
-            <n-input v-model:value="formValue.phone" placeholder="Phone Number" />
-          </n-form-item>
-          <n-form-item>
-            <n-button type="primary" @click="handleSubmit">
-              Validate
+  <div class="container">
+    <n-space justify="center">
+      <div>
+        <!--  Form  -->
+        <n-layout>
+          <n-form
+              ref="formRef"
+              label-placement="left"
+              :label-width="120"
+              :model="formValue"
+              :rules="rules"
+              size="large"
+          >
+            <n-space vertical size="small" class="mainForm">
+              <n-form-item label="請假人姓名" path="name">
+                <n-input v-model:value="formValue.name" placeholder="姓名" />
+              </n-form-item>
+
+              <n-space justify="end">
+                <n-checkbox
+                    style="width: 100%;"
+                    size="large"
+                    label="全天"
+                    v-model:checked="isAllDay"
+                    @update="onIsAllDayChanged" />
+              </n-space>
+
+              <n-form-item label="請假時間" path="name">
+                <n-date-picker
+                    format="yyyy-MM-dd HH:mm"
+                    :time-picker-props="timePickerProps"
+                    :type="isAllDay?'daterange':'datetimerange'"
+                    clearable
+                    :actions="['clear', 'confirm']"
+                    v-model:value="timestamp"
+                />
+              </n-form-item>
+
+              <!--  假別  -->
+              <n-form-item label="假別" path="reason">
+                <n-select v-model:value="formValue.leaveType" :options="leaveTypeOptions" />
+              </n-form-item>
+
+              <!--  事由  -->
+              <n-form-item label="事由" path="reason">
+                <n-input v-model:value="formValue.reason" type="textarea" placeholder="事由" />
+              </n-form-item>
+
+              <!--  代理人  -->
+              <n-form-item label="代理人" path="agentName">
+                <n-input v-model:value="formValue.agentName" type="text" placeholder="代理人" />
+              </n-form-item>
+
+              <n-form-item>
+                <n-space justify="end">
+                  <n-button type="primary" @click="handleSubmit">
+                    Generate Google Event
+                  </n-button>
+                </n-space>
+              </n-form-item>
+            </n-space>
+          </n-form>
+        </n-layout>
+
+        <!--  Result  -->
+        <n-layout>
+          <n-input v-model:value="resultValue" type="textarea" placeholder="" readonly />
+          <n-space justify="end">
+            <n-button type="primary" @click="handleMakeEvent" size="large">
+              Create Event
             </n-button>
-          </n-form-item>
-        </n-space>
-      </n-layout>
-    </n-layout>
-  </n-form>
+          </n-space>
+        </n-layout>
+      </div>
+    </n-space>
+  </div>
 </template>
 
 <style scoped>
-.mainForm {
-  width: 100%;
-  max-width: 400px;
+.container {
+  padding: 24px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
+
+.container div {
+  width: 500px;
 }
 </style>
